6d9e3c6359cbd1621631585865f23366f790242a
mauricio <mauricio>
2024-07-20 17:28:43 +0000

    feat: Add console command to update the app
    This commit adds a new console command `app:update` to update the application. The command performs several tasks including fetching the latest commit, updating the database, updating dependencies, updating node modules, building assets, and optimizing the application. It also handles any errors that may occur during the update process.

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,59 @@
+<?php
+
+// app/Console/Commands/Test.php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the app';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Starting update process');
+
+        try {
+            $this->runProcess('git pull', 'Fetching the last commit', 'Last commit fetched');
+            $this->runProcess('php artisan migrate --force', 'Updating database', 'Database updated');
+            $this->runProcess('composer install', 'Updating dependencies', 'Dependencies updated');
+            $this->runProcess('npm install', 'Updating node modules', 'Node modules updated');
+            $this->runProcess('npm run build', 'Building assets', 'Assets built');
+            $this->runProcess('php artisan optimize', 'Optimizing application', 'Application optimized');
+            
+            $this->info('App updated successfully');
+        } catch (\Throwable $th) {
+            $this->error('Update process failed: ' . $th->getMessage());
+        }
+    }
+
+    private function runProcess($command, $startMessage, $successMessage)
+    {
+        $this->info($startMessage);
+        $process = Process::fromShellCommandline($command);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new \Exception($process->getErrorOutput());
+        }
+
+        $this->info($successMessage);
+    }
+}
