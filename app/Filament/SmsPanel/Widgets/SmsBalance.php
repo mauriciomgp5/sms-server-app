c7b871106dc5188c4ce7bb021588f71c91d19813
mauricio <mauriciomgp5@gmail.com>
2024-07-25 14:13:15 +0000

    Update SmsBalance widget to include status enum for sent and delivered messages

--- app/Filament/SmsPanel/Widgets/SmsBalance.php
+++ app/Filament/SmsPanel/Widgets/SmsBalance.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\SmsPanel\Widgets;
 
+use App\Enums\SmsLog\StatusEnum;
 use App\Models\SmsLog;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -11,7 +12,7 @@ class SmsBalance extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Enviados', SmsLog::where('status', 'sent')->where('company_id', session('company')?->id)->count()),
+            Stat::make('Enviados', SmsLog::where(fn ($query) => $query->where('status', StatusEnum::Sent->value)->orWhere('status', StatusEnum::Delivered))->where('company_id', session('company')?->id)->count()),
             //calculo das respostas em porcentagem
             Stat::make('Respostas', SmsLog::where('status', 'response')->where('company_id', session('company')?->id)->count()),
             Stat::make('Saldo', auth()->user()->companies->first()?->balance?->balance ? 'R$ ' . number_format(auth()->user()->companies->first()?->balance->balance, 2, ',', '.') : 'R$ 0,00'),
